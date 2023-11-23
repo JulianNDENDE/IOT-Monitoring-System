e824551312818b657c5cdf47c7ca7f8797981764
Arnaud Ho Chouck Foune <arnaud.ho-chouck-foune@epitech.eu>
2023-11-23 09:51:56 +0100

    camera screenshot then send in a discord webhook

--- camera.py
+++ camera.py
@@ -1,11 +1,16 @@
 import cv2
 import time
+import requests
+from discord_webhook import DiscordWebhook
 
 # Load the pre-trained MobileNet SSD model and its configuration
 net = cv2.dnn.readNetFromCaffe(
     "deploy.prototxt",
     "mobilenet_iter_73000.caffemodel"
 )
+
+# Discord webhook URL
+webhook_url = 'https://discord.com/api/webhooks/1177142622683414618/Uw5ezUS39aw0uNGlVx_uSWFXQL3OHpycOD38nBVKP3lAICTqLHS3cz2fp-ju9EkgzvhE'
 
 # Open a video capture object (0 for default camera)
 cap = cv2.VideoCapture(0)
@@ -30,12 +35,16 @@ while True:
     # Perform object detection
     detections = net.forward()
 
+    person_found = False
+
     # Loop over the detections
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
 
         # If confidence is above a certain threshold (e.g., 0.2)
-        if confidence > 0.55:
+        if confidence > 0.80:
+            person_found = True
+
             # Get the coordinates of the bounding box
             box = detections[0, 0, i, 3:7] * [frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]]
             (startX, startY, endX, endY) = box.astype("int")
@@ -47,6 +56,16 @@ while True:
 
     # Display the resulting frame
     cv2.imshow('Full-Body Detection', frame)
+
+    # If a person is found, save the frame as a screenshot
+    if person_found:
+        cv2.imwrite('screenshot.png', frame)
+
+        # Send the screenshot to Discord
+        webhook = DiscordWebhook(url=webhook_url)
+        with open('screenshot.png', 'rb') as f:
+            webhook.add_file(file=f.read(), filename='screenshot.png')
+        webhook.execute()
 
     # Calculate the processing time
     processing_time = time.time() - start_time
